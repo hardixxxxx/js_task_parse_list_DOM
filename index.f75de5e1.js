0d98aaca8d7b27021f90e681d35b3ded76bb52e5
Anton Polishchuk <hardixxxxx@gmail.com>
2024-06-05 22:09:49 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const list = document.querySelector("ul");
+const salaryToNumber = (salary)=>+salary.replace(/[$,]/g, "");
+function sortList(ul) {
+    const items = [
+        ...ul.children
+    ].sort((item1, item2)=>salaryToNumber(item2.dataset.salary) - salaryToNumber(item1.dataset.salary));
+    items.forEach((el)=>list.append(el));
+}
+function getEmployees(ul) {
+    return [
+        ...ul.children
+    ].map((el)=>({
+            name: el.textContent.trim(),
+            position: el.dataset.position,
+            salary: el.dataset.salary,
+            age: el.dataset.age
+        }));
+}
+sortList(list);
+getEmployees(list);
+
+//# sourceMappingURL=index.f75de5e1.js.map
